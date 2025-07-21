8b6aa93c83d0cad35d47b457cbb7397b4e70b534
Dwyyee_ <arsyadlegendary@gmail.com>
2025-07-21 15:13:18 +0700

    Delete utils directory

--- utils/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// utils/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ebzevljxwmvozqmudxxe.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViemV2bGp4d212b3pxbXVkeHhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNzYyMzYsImV4cCI6MjA1OTk1MjIzNn0.mN9zWuVlQyk3k_kbwf406NQ5T8FO1AvWaMyvMRT9y00'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
